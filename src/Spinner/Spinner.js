f762324cfc15da8f4a9bfee83a998cc8ca091ad6
Kamila <kamilakiedrzynek@gmail.com>
2021-02-03 20:53:59 +0100

    Create Spinner component with bars placed in a circle

---
+++ src/Spinner/Spinner.js
@@ -0,0 +1,22 @@
+import './Spinner.css';
+
+function Spinner() {
+    return (
+        <div className="Spinner">
+            <div className="bar deg30"></div>
+            <div className="bar deg60"></div>
+            <div className="bar deg90"></div>
+            <div className="bar deg120"></div>
+            <div className="bar deg150"></div>
+            <div className="bar deg180"></div>
+            <div className="bar deg210"></div>
+            <div className="bar deg240"></div>
+            <div className="bar deg270"></div>
+            <div className="bar deg300"></div>
+            <div className="bar deg330"></div>
+            <div className="bar deg360"></div>
+        </div>
+    );
+}
+
+export default Spinner;
